ad37cbe0ba82095332200d3127e06f01aeb0c52f
rohan.john <rohan.john@firmusoft.co.in>
2025-04-11 11:05:10 +0530

    1. Unable to apply a style from toolbar to multiple selected cells in a table.

--- src/applyMark.ts
+++ src/applyMark.ts
@@ -212,7 +212,7 @@ export function updateMarksAttrs(markType: MarkType, tr: Transform, state: Edito
   let _startPos = startPos?.pos;
 
   // Traverse upwards to ensure we reach the full paragraph from selection start
-  while (startPos?.parent?.type?.name !== 'paragraph' && startPos?.depth > 0) {
+  while (startPos?.parent?.type?.name !== 'paragraph' && startPos?.parent?.type?.name !== 'table_cell' && startPos?.depth > 0) {
     _startPos = startPos.before();
   }
 
@@ -292,7 +292,7 @@ export function updateToggleMarks(markType: MarkType, tr: Transform, state: Edit
   let _startPos = startPos.pos;
   const { schema } = state;
   // Traverse upwards to ensure we reach the full paragraph from selection start
-  while (startPos.parent.type.name !== 'paragraph' && startPos.depth > 0) {
+  while (startPos.parent.type.name !== 'paragraph' && startPos?.parent?.type?.name !== 'table_cell' && startPos.depth > 0) {
     _startPos = startPos.before();
   }
 
